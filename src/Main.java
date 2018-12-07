0e1d901f525e29ce6acb5559b2de1eaaeeb5e40f
ChristianBendtsen <chri04cm@stud.kea.dk>
2018-12-07 11:19:47 +0100

    Merge branch 'master' of https://github.com/christianbendtsen/untitled6
                         # Conflicts:
                         #	src/Arrangement.java
                         #	src/Event.java
                         #	src/Sekretær.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main
 {
     public static void main(String[] args) {
-        StartMenu startMenu = new StartMenu();
         Personale.addSekretærDummyData();
         Personale.addFacilitatorDummyData();
-        startMenu.validerBruger();
+        StartMenu.validerBruger();
+
+
 
 
     }
